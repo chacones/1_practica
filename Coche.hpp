e148e015df616d1370a2f859f58b349f3e79a5c7
Laura Chacón <laurachacon92@gmail.com>
2012-01-30 10:48:37 +0100

    ficheros hpp

---
+++ Coche.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class Coche {
+
+private:
+  int matricula;
+
+public:
+  Coche();
+  ~Coche();
+  int get_matricula();
+  void leer_coche();
+
+};
---
+++ Parking.hpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <list>
+#include "Coche.hpp"
+using namespace std;
+
+class Parking {
+
+private:
+  list<Coche> aparcamiento;
+  int plazas_libres;
+
+public:
+  Parking();
+  ~Parking();
+  void meter_coche();
+  void sacar_coche();
+  int get_plazas_libres();
+
+};
