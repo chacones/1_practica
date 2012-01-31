3afbb19a2a9d1d836e5d0d02ff878d0c077b9236
Laura Chacón <laurachacon92@gmail.com>
2012-01-31 10:58:52 +0100

    ultima actualización

--- Parking.cpp
+++ Parking.cpp
@@ -7,14 +7,26 @@ Parking::~Parking() {}
 void Parking::meter_coche(int num_plazas) {
   Coche c;
   c.leer_coche();
-  list<Coche>::iterator it = aparcamiento.begin();
-  it = num_plazas - plazas_libres + 1;
+  list<Coche>::iterator it = aparcamiento.begin();   
+  aparcamiento.insert(it,c);
+  plazas_libres--;
 }
 
 void Parking::sacar_coche(int num_plazas) {
   Coche c;
   c.leer_coche();
-
+  list<Coche>::iterator it = aparcamiento.begin();   
+  bool trobat = false;
+  while(it != aparcamiento.end() and not trobat) {
+    Coche aux = *it;
+    if(c.get_matricula() == it.get_matricula()) {
+      aparcamiento.erase(it);
+      plazas_libres++;
+      trobat = true;
+    } 
+    ++it;
+    
+  }
 }
 
 int Parking::get_plazas_libres() {
